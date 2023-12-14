1d77e35534de7dc8761aae7f040897699a43dc4e
Ricardo Guerrero <richi_gro@hotmail.com>
2023-12-13 20:16:26 -0600

    Added a pause menu

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -2,9 +2,18 @@ extends Node
 
 @export var end_screen_scene: PackedScene
 
+var pause_menu_scene = preload("res://scenes/ui/pause_menu.tscn")
+
 func _ready():
 	$%Player.health_component.died.connect(on_player_died)
 
+
+# Listen for any un-handled events that happen in our main scene
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		add_child(pause_menu_scene.instantiate())
+		# mark current event as handled to stop event from bubling up to parent nodes
+		get_tree().root.set_input_as_handled()
 
 func on_player_died():
 	var end_screen_instance = end_screen_scene.instantiate()
